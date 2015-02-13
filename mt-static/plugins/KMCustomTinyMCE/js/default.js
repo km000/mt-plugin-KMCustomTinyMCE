011c0250b1c65a447f0b18f8a13fb243a985590b
Koji Matsumoto <libra804.sakrua@gmail.com>
2015-02-13 16:21:10 +0900

    微細な仕様変更およびカスタムフィールドのウィジウィグ化
    次の変更を実施。
    ・一部プラグインデータの内容を外部ファイルを参照する形とした。
    ・記事の編集画面におけるカスタムフィールドをウィジウィグ化する実装を追加した。

---
+++ mt-static/plugins/KMCustomTinyMCE/js/default.js
@@ -0,0 +1,75 @@
+
+(function($) {
+  var config   = MT.Editor.TinyMCE.config;
+  var base_url = StaticURI + 'plugins/KMCustomTinyMCE/';
+//
+//ツールボタンの3行目に表示するコントロールの設定。
+  buttons3 = (config.plugin_mt_wysiwyg_buttons3 || '')
+    + ',fontsizeselect'
+    + ',tablecontrols';
+//
+  $.extend(config, {
+//読み込むTinyMCE向けプラグインの設定。
+    plugins: config.plugins + ',table',
+//
+//エディタの編集領域に読み込むスタイルの設定。
+    content_css: config.content_css + ',' + base_url + 'skin/content.css',
+//
+//ツールボタンの3行目に表示する項目の設定。
+    plugin_mt_wysiwyg_buttons3 : buttons3,
+//
+//ツールボタンの見出しの選択肢の設定。
+    theme_advanced_blockformats : "h2,h3,h4,h5,h6,p,pre",
+//
+//ツールボタンのフォントサイズの選択肢の設定。
+    theme_advanced_font_sizes : "10px,12px,14px,16px,24px,36px",
+//
+//挿入されるアイテムのURLをルート相対にする。
+    convert_urls : true,
+    relative_urls : false
+  });
+//
+//>>ツールボタンよりtableを操作する機能の設定。
+  $.extend(config.plugin_mt_inlinepopups_window_sizes, {
+      'table/table.htm': {
+          width: 600,
+          height: 300
+      },
+      'table/row.htm': {
+          width: 450,
+          height: 300
+      },
+      'table/cell.htm': {
+          width: 450,
+          height: 300
+      },
+      'table/merge_cells.htm': {
+          width: 250,
+          height: 140
+      }
+  });
+//
+//>> カスタムフィールドにTinyMCEを適用する
+if( mtappVars.screen_id == 'edit-entry' ){
+//
+    // カスタムフィールドで追加したテキストエリアにTinyMCEを適用
+    var _esm = MT.App.EditorStrategy.Multi;
+    var _create = _esm.prototype.create;
+    var _set = _esm.prototype.set;
+//
+    _esm.prototype.create = function(app, ids, format) {
+        jQuery('textarea[id^="customfield_"]').each(function(i) {
+            ids.push(this.id);
+        });
+        jQuery('textarea[id="excerpt"]').each(function(i) {
+            ids.push(this.id);
+        });
+        _create.apply(this, arguments);
+    };
+//
+}
+//<<カスタムフィールドにTinyMCEを適用する
+
+
+})(jQuery);
+
